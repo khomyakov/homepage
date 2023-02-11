7e1fb8646ea91f2962d5b22a1b41fade6940c985
Alexander Khomyakov <akhomyakov@gmail.com>
2023-02-11 14:26:54 +0200

    Minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App container">
-      <h3 className="message"><strong>Alexander Khomyakov</strong><br> - adores spaceships 🚀 and is going to deploy the new version of this page soon.</h3>
+      <h3 className="message"><strong>Alexander Khomyakov</strong><br/> - adores spaceships 🚀 and is going to deploy the new version of this page soon.</h3>
     </div>
   )
 }
